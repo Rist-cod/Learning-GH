29888db521034afb80304e289636408a4d38933c
Rist-cod <129657775+Rist-cod@users.noreply.github.com>
2023-04-02 12:13:56 +0200

    Add Text hello

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("What's up codecrew")
+            Text("Hello")
         }
         .padding()
     }
